7d49fe3035714c5378990f60ccaaf360e248539a
viktormarinho <viktormpcs@gmail.com>
2022-12-19 11:36:01 -0300

    feat(internals): extract nats connection into own hook + prettier
    Extracted nats connection, sub and pub logic into its own hook so it is easier to support other
    pubsub options later (like redis)

--- src/useChannel.ts
+++ src/useChannel.ts
@@ -1,102 +1,129 @@
-import { MutationFunction, QueryFunction, QueryKey, useMutation, UseMutationOptions, UseMutationResult, useQuery, useQueryClient, UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
-import { connect, NatsConnection, NatsError, StringCodec, Subscription } from 'nats.ws';
-import React, { useCallback, useEffect, useState } from 'react'
+import {
+    MutationFunction,
+    QueryFunction,
+    QueryKey,
+    useMutation,
+    UseMutationOptions,
+    UseMutationResult,
+    useQuery,
+    useQueryClient,
+    UseQueryOptions,
+    UseQueryResult,
+} from "@tanstack/react-query";
+import { SubscriptionOptions } from "nats";
+import {
+    connect,
+    NatsConnection,
+    NatsError,
+    StringCodec,
+    Subscription,
+} from "nats.ws";
+import React, { useCallback, useEffect, useState } from "react";
 
 const sc = StringCodec();
 
-export const useChannel = (channel: string, onMessage: (err: NatsError | null, msg: string) => void) => {
-  const [nc, setConnection] = useState<NatsConnection>();
-  const [sub, setSubscription] = useState<Subscription>();
-  const [isConnecting, setIsConnecting] = useState<boolean>(true);
+const useNatsConnection = (servers: string | string[]) => {
+    const [nc, setConnection] = useState<NatsConnection>();
 
-  useEffect(() => {
-    connect({ servers: ['ws://localhost:4444'] })
-    .then(connection => {
-      setConnection(connection);
-    })
-  }, []);
-
-  useEffect(() => {
-    if (nc) {
-        console.log('connecting to channel', channel)
-        const sub = nc.subscribe(channel, { 
-            callback: (err, msg) => {
-                onMessage(err, sc.decode(msg.data));
-            }
+    useEffect(() => {
+        connect({ servers }).then((connection) => {
+            setConnection(connection);
         });
-        setSubscription(sub);
-        setIsConnecting(false);
-    }
+    }, []);
 
-    return () => {
-      sub?.unsubscribe();
-    }
-  }, [nc]);
+    const subscribe = useCallback((channel: string, opts?: SubscriptionOptions) => {
+        return nc?.subscribe(channel, opts);
+    }, [nc])
 
-  const publish = (message: string) => {
-    nc?.publish(channel, sc.encode(message));
-  }
+    const publish = useCallback((channel: string, message: string) => {
+        nc?.publish(channel, sc.encode(message));
+    }, [nc])
 
-  return { nc, sub, isConnecting, publish };
-}
+    return { connection: nc, subscribe, publish };
+};
+
+export const useChannel = (
+    channel: string,
+    onMessage: (err: NatsError | null, msg: string) => void
+) => {
+    const { connection, subscribe, publish } = useNatsConnection(["ws://localhost:4444"]);
+    const [sub, setSubscription] = useState<Subscription>();
+    const [isConnecting, setIsConnecting] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (connection) {
+            console.log("connecting to channel", channel);
+            const sub = subscribe(channel, {
+                callback: (err, msg) => {
+                    onMessage(err, sc.decode(msg.data));
+                },
+            });
+            setSubscription(sub);
+            setIsConnecting(false);
+        }
+
+        return () => {
+            sub?.unsubscribe();
+        };
+    }, [connection]);
+
+    const pub = (message: string) => {
+        publish(channel, message);
+    };
+
+    return { connection, isConnecting, sub, pub };
+};
 
 export const useRealtimeChannel: <QueryT, MutationT, MutationInputT>(
     queryKey: QueryKey,
     queryFn: QueryFunction<QueryT>,
     mutationFn: MutationFunction<MutationT, MutationInputT>,
     opts?: {
-        query: UseQueryOptions<QueryT>,
-        mutation: UseMutationOptions<MutationT, any, MutationInputT, any>
+        query: UseQueryOptions<QueryT>;
+        mutation: UseMutationOptions<MutationT, any, MutationInputT, any>;
     }
 ) => {
-    query: UseQueryResult<QueryT>,
-    mutation: UseMutationResult<MutationT, any, MutationInputT, any>,
-    channel: ReturnType<typeof useChannel>
-} = (
-    queryKey, 
-    queryFn, 
-    mutationFn, 
-    opts
-) => {
+    query: UseQueryResult<QueryT>;
+    mutation: UseMutationResult<MutationT, any, MutationInputT, any>;
+    channel: ReturnType<typeof useChannel>;
+} = (queryKey, queryFn, mutationFn, opts) => {
     const client = useQueryClient();
     const query = useQuery(queryKey, {
         queryFn,
-        ...opts?.query
+        ...opts?.query,
     });
+    
     const channel = useChannel(JSON.stringify(queryKey), (err, msg) => {
-        console.log('invalidating query with the key', queryKey)
+        console.log("invalidating query with the key", queryKey);
         client.invalidateQueries(queryKey);
-    })
+    });
 
     const invalidateAllClients = useCallback(() => {
-        channel.nc?.publish(JSON.stringify(queryKey), sc.encode(JSON.stringify(queryKey)));
+        channel.pub(JSON.stringify(queryKey));
     }, [channel]);
 
-    const mutation = useMutation(
-        mutationFn,
-        {
-            onSuccess: (data, variables, context) => {
-                invalidateAllClients();
-                if (opts?.mutation.onSuccess) {
-                    opts?.mutation.onSuccess(data, variables, context);
-                }
-            },
-            ...opts?.mutation
-        }
-    )
+    const mutation = useMutation(mutationFn, {
+        onSuccess: (data, variables, context) => {
+            invalidateAllClients();
+            if (opts?.mutation.onSuccess) {
+                opts?.mutation.onSuccess(data, variables, context);
+            }
+        },
+        ...opts?.mutation,
+    });
 
-    return {query, mutation, channel };
-}
+    return { query, mutation, channel };
+};
 
-export const useRealtimeInvalidator = (key: QueryKey) => {
+export const useRealtimeInvalidator = (queryKey: QueryKey) => {
     const client = useQueryClient();
-    const channel = useChannel(JSON.stringify(key), (err, msg) => {
-        client.invalidateQueries(key);
-    })
+    const channel = useChannel(JSON.stringify(queryKey), (err, msg) => {
+        client.invalidateQueries(queryKey);
+    });
 
     const invalidateAllClients = useCallback(() => {
-        channel.publish(JSON.stringify(key));
-    }, [channel])
+        channel.pub(JSON.stringify(queryKey));
+    }, [channel]);
 
-    return {...channel, invalidateAllClients};
-}
+    return { ...channel, invalidateAllClients };
+};
